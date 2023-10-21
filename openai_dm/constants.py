4a00ef6681b17bd76449d700c16c52a1a3e7b85b
Dave Zornek <david.zornek@gmail.com>
2023-10-20 23:55:41 -0500

    Add class tool

--- openai_dm/constants.py
+++ openai_dm/constants.py
@@ -1,4 +1,4 @@
-from openai_dm.tools import RaceTool
+from openai_dm.tools import RaceTool, ClassTool
 
 COST_PER_1000_TOKENS = {
     "text-davinci-003": {
@@ -19,10 +19,12 @@ NODE_RULES = {
     "race": [
         "After the player indicates their choice of race, update the character's race.",
     ],
-    "class_": [],
+    "class_": [
+        "After the player indicates their choice of class, update the character's class.",
+    ],
 }
 
 NODE_TOOLS = {
     "race": [RaceTool],
-    "class_": [],
+    "class_": [ClassTool],
 }
