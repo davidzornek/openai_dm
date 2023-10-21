6a47f51c309ba9ea69f0a966b8db94b25a9816ed
Dave Zornek <david.zornek@gmail.com>
2023-10-20 21:32:50 -0500

    Conversation now starts with the race node under the new structure

--- openai_dm/conversation.py
+++ openai_dm/conversation.py
@@ -1,13 +1,14 @@
+from attr import define, field, Factory
 import logging
 
-from griptape.rules import Rule, Ruleset
-from griptape.drivers import OpenAiChatPromptDriver
+# from griptape.rules import Rule, Ruleset
+# from griptape.drivers import OpenAiChatPromptDriver
 from griptape.memory.structure import ConversationMemory
 
 from openai_dm.dm_kit import DMAgent
 from openai_dm.character_sheet import Character
 from openai_dm.tools import CharacterSheetUpdater
-from openai_dm.constants import NODE_RULES
+from openai_dm.constants import NODE_TOOLS
 
 CONVERSATION_GRAPH = {
     "race": ["class_"],
@@ -18,92 +19,73 @@ CONVERSATION_GRAPH = {
 }
 
 
-class Conversation:
-    def __init__(
-        self,
-        name: str = "AI Dungeon Master",
-        gpt4: bool = True,
-        max_tokens: int = 500,
-        logger_level: int = logging.INFO,
-        starting_node: str = "race",
-    ):
-        self.name = name
-        self.main_rules = Ruleset(
-            name="Character Creation Rules",
-            rules=[
-                Rule(
-                    """You are the dungeon master of a 5e campaign, assisting the user
-                     with character creation."""
-                ),
-                Rule(
-                    """Be brief when you response; give minimal but complete information
-                    unless the user asks for more."""
-                ),
-                Rule("""Discuss only topics related to D&D."""),
-                Rule("""Whenever you see the word 'class_', treat it at 'class'."""),
-            ],
-        )
-        self.gpt4 = gpt4
-        self.max_tokens = max_tokens
-        self.logger_level = logger_level
-        self.agent = None
-        self.test = None
-        self.character_sheet = Character()
-        self.memory = ConversationMemory()
-        self.first_run = True
-        self._start_node(node_name=starting_node)
-        # self.agent.run("Begin.")
-        # self.agent.run(f'''
-        #     Introduce yourself. Tell the user you're here to help them with creating
-        #     a character, as well as that we're starting with selecting {starting_node}.
-        #     Give them any basic information they might need to get started with that.
-        # ''')
+@define
+class CharacterCreationConversation:
+    name: str = field(default="AI Dungeon Master")
+    gpt4: bool = field(default=True)
+    max_tokens: int = field(default=500)
+    logger_level: int = field(default=logging.INFO)
+    starting_node: str = field(default="race")
+    character_sheet: Character = field(default=Factory(lambda: Character()))
+    agent: DMAgent = field(default=None)
+    memory: ConversationMemory = field(default=Factory(lambda: ConversationMemory()))
 
-    def _start_node(self, node_name: str):
-        self.current_node = node_name
-        if node_name[-1] == "_":
-            node_name = node_name[:-1]
-
-        node_initialization_rules = [
-            f"""You are helping the player choose a {node_name}. Once they have chosen a
-            {node_name} update the character sheet with the new {node_name}""",
-            '''After updating the character sheet say only "change_node"''',
-        ]
-        node_rules = [self.main_rules]
-        node_rules.append(
-            Ruleset(
-                name="node_initalization_rules",
-                rules=[Rule(x) for x in node_initialization_rules],
-            )
-        )
-        node_rules.append(
-            Ruleset(
-                name=f"{node_name} rules",
-                rules=[Rule(x) for x in NODE_RULES[self.current_node]],
-            )
-        )
-
+    def __attrs_post_init__(self):
         self.agent = DMAgent(
             character_sheet=self.character_sheet,
-            rulesets=node_rules,
-            logger_level=self.logger_level,
-            prompt_driver=OpenAiChatPromptDriver(
-                model="gpt-4" if self.gpt4 else "gpt-3.5-turbo-0613",
-                max_tokens=self.max_tokens,
-            ),
+            tools=NODE_TOOLS[self.starting_node],
+            node=self.starting_node,
             memory=self.memory,
+            logger_level=self.logger_level,
         )
         response = self.agent.run(
-            f"""
-            If first_run==True, then introduce yourself, tell them
-            you're here to help them with character creation, as well as what you're
-            going to start with. Regarldess of whether this is the first run,
-            give them any basic information they might need for selecting a {node_name}
-            first_run=={self.first_run}
-            """
+            "Introduce yourself and introduce the player the character creation."
         )
         print(response.output.value)
-        return response
+
+    #     self._start_node(node_name=self.starting_node)
+    #     if node_name[-1] == "_":
+    #         node_name = node_name[:-1]
+
+    # def _start_node(self, node_name: str):
+    #     self.current_node = node_name
+    #     self.agent
+
+    #     node_rules = [self.main_rules]
+    #     node_rules.append(
+    #         Ruleset(
+    #             name="node_initalization_rules",
+    #             rules=[Rule(x) for x in node_initialization_rules],
+    #         )
+    #     )
+    #     node_rules.append(
+    #         Ruleset(
+    #             name=f"{node_name} rules",
+    #             rules=[Rule(x) for x in NODE_RULES[self.current_node]],
+    #         )
+    #     )
+
+    #     self.agent = DMAgent(
+    #         character_sheet=self.character_sheet,
+    #         rulesets=node_rules,
+    #         logger_level=self.logger_level,
+    #         prompt_driver=OpenAiChatPromptDriver(
+    #             model="gpt-4" if self.gpt4 else "gpt-3.5-turbo-0613",
+    #             max_tokens=self.max_tokens,
+    #         ),
+    #         memory=self.memory,
+    #     )
+    #     response = self.agent.run(
+    #         f"""
+    #         If first_run==True, then introduce yourself, tell them
+    #         you're here to help them with character creation, as well as what you're
+    #         going to start with. Regarldess of whether this is the first run,
+    #         give them any basic information they might need for selecting a {node_name}
+    #         first_run=={self.first_run}
+    #         """
+    #     )
+    #     print(response.output.value)
+    #     return response
 
     def run(self, user_input: str):
         response = self.agent.run(user_input)
