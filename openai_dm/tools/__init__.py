378b004ff20ff1fbef58d003b493439cf1629bc0
Dave Zornek <david.zornek@gmail.com>
2023-10-23 00:09:46 -0500

    finish up '

--- openai_dm/tools/__init__.py
+++ openai_dm/tools/__init__.py
@@ -1,6 +1,6 @@
+from .base_sheet_update_tool import BaseSheetUpdateTool
 from .ability_score_tool.tool import AbilityScoreTool
 from .background_tool.tool import BackgroundTool
-from .base_sheet_update_tool.tool import BaseSheetUpdateTool
 from .character_sheet_inspector.tool import CharacterSheetInspector
 from .class_tool.tool import ClassTool
 from .race_tool.tool import RaceTool
